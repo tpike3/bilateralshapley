c8b90d2b04fb9e4deb6bf53d33ccc772b89aa409
Tom Pike <tpike3@gmu.edu>
2018-01-30 11:22:04 -0400

    Added setup file and init file

---
+++ __init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Mesa Agent-Based Modeling Framework
+
+Core Objects: Model, and Agent.
+
+"""
+import datetime
+
+
+__title__ = 'Bilateral Shapley'
+__version__ = '0.0,1'
+__license__ = 'MIT'
+__copyright__ = 'Copyright %s Tom Pike' % datetime.date.today().year
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+
+
+from setuptools import setup, find_packages
+from codecs import open
+
+
+#get version from init file
+version = ''
+with open('bishapley/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+#get the readme file for the long description below--optional
+with open('README.rst', 'rb', encoding='utf-8') as f:
+    readme = f.read()
+
+# see https://github.com/pypa/sampleproject/blob/master/setup.py for explanation of each parameter and links
+setup(
+    name='Bilateral Shapley',
+    version=version,
+    description="Employs Bilateral Shapley Value from coalition game theory as a tool to assess agent coalition formation",
+    long_description=readme,
+    url='https://github.com/tpike3/bilateralshapley',
+    author='Tom Pike',
+    author_email='tpike3@gmu.edu',
+    classifiers=[
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Life',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License License',
+        'Operating System :: OS Independent',
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+    ],
+    keywords='agent based modeling model ABM simulation multi-agent coaltion game theory',
+    py_modules = ["BilateralShapley", "example"],
+    #for more elaborate projects with directories of files such as tests etc
+    #packages=find_packages(),
+    #package_data={'mesa': ['visualization/templates/*.html', 'visualization/templates/css/*',
+    #                       'visualization/templates/fonts/*', 'visualization/templates/js/*']},
+    include_package_data=True,
+    install_requires=['networkx','mesa'],
+)
